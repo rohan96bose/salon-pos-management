0c263f456ee3cef66df2b79565d91eb32ee8ab22
Rohan Bose <62739334+rohan96bose@users.noreply.github.com>
2025-08-14 18:54:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- salon-management-frontend/index.html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="refresh" content="0; URL=pages/login.php" />
+  <title>Salon Management System</title>
+</head>
+<body>
+  <p>Redirecting to login page...</p>
+</body>
+</html>
